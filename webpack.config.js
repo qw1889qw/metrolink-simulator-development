4e2fd86dd2dee72fbf7d18d5733153a7a9076b17
questionedandmarked <qw1889qw@gmail.com>
2020-05-26 11:36:16 -0700

    added basic webpack configuration for handling styles, integration between webpack & stylelint, and npm script to run webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new StylelintPlugin({
+      files: ['src/styles/*.css', 'src/styles/*.scss']
+    })
+  ]
+};
