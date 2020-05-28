20d36cdbc26a95fb00647e1b6f5142ed797f3164
questionedandmarked <qw1889qw@gmail.com>
2020-05-28 10:02:05 -0700

    set up basic ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  extends: [
+    'airbnb-base'
+  ]
+};
